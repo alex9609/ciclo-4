049c3234e47abf62fb78b73975db318ba91deb3e
Alex <alrodriguez41@poligran.edu.co>
2022-10-20 08:03:56 -0500

    first commit

---
+++ proyecto_back/app.js
@@ -0,0 +1,48 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+//importar el database.js
+var database = require('./config/database')//Nombre de archivo sin extensiones
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+var empleadosRouter = require('./routes/empleados.router');
+
+var app = express();
+
+// view engine setup
+
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+//MongoConnection
+database.mongoConnect();
+//Router
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+app.use('/empleados',empleadosRouter)
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
---
+++ proyecto_back/routes/empleados.router.js
@@ -0,0 +1,4 @@
+const express = require('express')
+
+const router = express.Router();
+const empleadosController = require("../controllers/empleados.controller"); //Ruta del controlador
